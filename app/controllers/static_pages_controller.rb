16e18cef287ffc4653359fbd9d867ce4f752b370
Tafang Joshua <tafodinho@gmail.com>
2019-07-06 10:07:07 +0100

    added changes to view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
+  before_action :check_login
+
   def home
-    flash.now[:success] = "Welcome to the connect-me homepage"
+    
   end
+
+  private 
+    def check_login 
+      redirect_to login_path if !user_signed_in?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end 
 end
