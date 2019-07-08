f2e70241c6806df7b9b73360e548dd8c2b9c4861
Tafang Joshua <tafodinho@gmail.com>
2019-07-08 22:25:16 +0100

    added columns to migration files

--- db/migrate/20190708164015_create_likes.rb
+++ db/migrate/20190708164015_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-
+      t.belongs_to :user 
+      t.belongs_to :comment
       t.timestamps
     end
   end
