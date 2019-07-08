faab82ebc01f26c83412577ce678774d4a7d88b9
offorma <offorma@gmail.com>
2019-07-08 17:22:36 +0100

    setting up user model to store returned data from facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "271992697000235", "7f0e0042c585f2df2df8fa18c10e442f", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
