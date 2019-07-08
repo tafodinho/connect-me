072c8a39c10b9444e32b4e86ef0e7d66b12267b6
Tafang Joshua <tafodinho@gmail.com>
2019-07-08 18:19:39 +0100

    added new models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :friendships 
+  has_many :post 
+  has_many :comments 
+  has_many :likes 
+  has_many :friends, through: :friendship
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
---
+++ db/migrate/20190708164026_create_friendships.rb
@@ -0,0 +1,10 @@
+class CreateFriendships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :friendships do |t|
+      t.belongs_to :user
+      t.belongs_to :friend, class: 'User'
+      t.integer :status
+      t.timestamps
+    end
+  end
+end
