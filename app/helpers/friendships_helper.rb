48a4abbd329fc4e2a504cd24bfd9ebfa5b4b31b0
Tafang Joshua <tafodinho@gmail.com>
2019-07-12 10:51:26 +0100

    added ability for user to send friend request

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,7 @@
 module FriendshipsHelper
+    def requested(user_id)
+        request = Friendship.where(user_id: current_user, friend_id: user_id, status: 0)
+
+        return true if request.count > 0 else return false
+    end
 end
