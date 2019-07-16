9135f4374a85fc23ed7905b967c1badf79021903
offorma <offorma@gmail.com>
2019-07-16 11:31:00 +0100

    fixed  using scopes in friend requests

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,13 +1,3 @@
 module FriendshipsHelper
-    def requested(user_id)
-        request = Friendship.where(user_id: current_user, friend_id: user_id, status: 0)
-
-        return true if request.count > 0 else return false
-    end
-
-    def is_friend(user_id) 
-        request = Friendship.where(friend_id: current_user, user_id: user_id, status: 1).or(Friendship.where(user_id: current_user, friend_id: user_id, status: 1))
-
-        return true if request.count > 0 else return false
-    end
+  
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,8 @@ class Friendship < ApplicationRecord
     belongs_to :friend, class_name: "User"
 
     validates_uniqueness_of :user_id, :scope => [:friend_id]
+
+    scope :requested, -> (user_id, current_user){ where(user_id: current_user, friend_id: user_id, status: 0)}
+    scope :is_friend, -> (user_id, current_user){ where(friend_id: current_user, user_id: user_id, status: 1).or(Friendship.where(user_id: current_user, friend_id: user_id, status: 1))}
+
 end
